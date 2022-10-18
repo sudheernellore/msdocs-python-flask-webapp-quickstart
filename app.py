b41b93fad6696df7a310e21ac1d0e19d4b357b33
Sudheer Kumar Nellore <46022809+sudheernellore@users.noreply.github.com>
2022-10-18 14:42:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello():
    name = request.form.get('name')
 
    if name:
-       print('Request for hello page received with name=%s' % name)
+       print('Request for hello page received with name=%s changed' % name)
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
